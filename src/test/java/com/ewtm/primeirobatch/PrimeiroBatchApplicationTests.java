83e0511f1a0203e03e60e61df9edf15c237482dc
Eduardo Mergulhao <eduardomergulhao@MacBook-Air-de-Eduardo.local>
2022-06-27 12:17:01 -0300

    first commit

---
+++ src/test/java/com/ewtm/primeirobatch/PrimeiroBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewtm.primeirobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
